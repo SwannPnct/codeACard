fb927fc89dcd0e9d1af5a04975473d330e015542
Swann Pincet <swann.pincet@gmail.com>
2024-02-18 12:20:28 +0100

    html dynamic wrapper and html sanitizer

--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -2,6 +2,7 @@
 	import { browser } from '$app/environment';
 	import { Spinner } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
+	import DOMPurify from 'dompurify';
 
 	export let title;
 	export let size = 'vistaPrintStandard';
@@ -19,34 +20,43 @@
 	};
 
 	let AceEditor;
+	let purifyHTML;
 	onMount(async () => {
 		if (browser) {
 			await import('brace');
 			await import('brace/mode/html');
-			await import('brace/mode/css');
-			await import('brace/theme/chrome');
+			await import('brace/theme/monokai');
 			AceEditor = (await import('svelte-ace')).AceEditor;
+
+			purifyHTML = DOMPurify(window);
 		}
 	});
 
-	let code = `<div class="border border-black border-dashed" style="width:${sizes[size].width};height:${sizes[size].height}">\n\n</div>`;
+	$: topWrapper = `<div class="border border-black border-dashed" style="width:${sizes[size].width};height:${sizes[size].height}">`;
+	const bottomWrapper = '</div>';
+
+	let code = `<div class="size-full">\n\n</div>`;
 </script>
 
 <div class="flex w-1/2 flex-col items-center">
 	<h2>
 		{title}
 	</h2>
-	{@html code}
-	<h3>HTML</h3>
+	{#if purifyHTML}
+		{@html purifyHTML.sanitize(topWrapper + code + bottomWrapper)}
+	{/if}
+	<h3>HTML + Tailwind CSS</h3>
 	{#if AceEditor}
+		<code class="self-start text-xs text-gray-400">{topWrapper}</code>
 		<svelte:component
 			this={AceEditor}
 			lang="html"
-			theme="chrome"
+			theme="monokai"
 			bind:value={code}
 			width="100%"
 			height="300px"
 		/>
+		<code class="self-start text-xs text-gray-400">{bottomWrapper}</code>
 	{:else}
 		<div class="flex items-center justify-center" style={`width: 100%; heigth: ${editorHeight}`}>
 			<Spinner size="10" />
